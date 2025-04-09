3d2f6514f7af895ed6002002ba65fe8db44ac617
Yasmin <yasminaraujoarantes@gmail.com>
2025-04-09 13:44:52 -0300

    [DEV-002]
    Commit inicial

---
+++ src/main/java/com/yasminarantes/consulta_credito/ConsultaCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.yasminarantes.consulta_credito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaCreditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yasminarantes/consulta_credito/ConsultaCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasminarantes.consulta_credito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
